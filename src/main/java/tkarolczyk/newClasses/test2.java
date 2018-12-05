e334fcb787415ea86b8492a1afd8e5c6148e57a0
Tomas <tobomabas@gmail.com>
2018-12-05 19:03:27 +0100

    [new] nowa klasa Main

--- src/main/java/tkarolczyk/newClasses/test2.java
+++ src/main/java/tkarolczyk/newClasses/test2.java
@@ -1,4 +1,12 @@
 package tkarolczyk.newClasses;
 
 public class test2 {
+
+    private int tomek;
+    private String name;
+
+    public test2(int tomek, String name) {
+        this.tomek = tomek;
+        this.name = name;
+    }
 }
